5080801fb901ba02c45176e5115e7a92f7933e2c
PBCDev <baochaupham4096@gmail.com>
2026-08-10 22:59:26 +0700

    nixvim: map Ctrl+Backspace = db

--- home/programs/nixvim/edit/keymaps/default.nix
+++ home/programs/nixvim/edit/keymaps/default.nix
@@ -4,4 +4,16 @@
     ./history.nix
     ./motions.nix
   ];
+
+  programs.nixvim.keymaps = [
+    {
+      mode = "i";
+      key = "<C-BS>";
+      action = "<cmd>normal! db<CR>";
+      options = {
+        silent = true;
+        desc = "Delete inner word with Ctrl+Backspace";
+      };
+    }
+  ];
 }
