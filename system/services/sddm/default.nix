db01c1bc3d9da46761eaba2a6bdf5913925658c3
PBCDev <baochaupham4096@gmail.com>
2026-08-11 14:25:02 +0700

    sddm: use theme astronaut

--- system/services/sddm/default.nix
+++ system/services/sddm/default.nix
@@ -1,3 +1,30 @@
+{ pkgs, ... }:
+let
+  sddm-astronaut = (pkgs.sddm-astronaut.override {
+    embeddedTheme = "astronaut";
+    themeConfig = {
+      HeaderTextColor = "#d5c4a1";
+      # Background = "Backgrounds/your-custom-background.png";
+    };
+  }).overrideAttrs (oldAttrs: {
+    installPhase = oldAttrs.installPhase; # ''
+    # chmod u+w $out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/
+    #cp ${./relative/path/to/your-custom-background.png} \
+    #  $out/share/sddm/themes/sddm-astronaut-theme/Backgrounds/your-custom-background.png
+    # '' ;
+  });
+in
 {
-  services.displayManager.sddm.enable = true;
+  environment.systemPackages = [ sddm-astronaut ];
+
+  services.displayManager.sddm = {
+    enable = true;
+
+    extraPackages = with pkgs; [
+      kdePackages.qtmultimedia
+    ];
+
+    theme = "sddm-astronaut-theme";
+  };
 }
+
