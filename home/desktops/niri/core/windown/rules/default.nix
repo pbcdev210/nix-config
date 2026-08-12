be509265e9bf056eec55e24f22621f31755b05c0
PBCDev <baochaupham4096@gmail.com>
2026-08-12 14:26:53 +0700

    niri(windown) rounded corners = 6px

--- home/desktops/niri/core/windown/rules/default.nix
+++ home/desktops/niri/core/windown/rules/default.nix
@@ -6,10 +6,10 @@
       {
         matches = [{ }];
         geometry-corner-radius = {
-          top-left = 2.0;
-          top-right = 2.0;
-          bottom-left = 2.0;
-          bottom-right = 2.0;
+          top-left = 6.0;
+          top-right = 6.0;
+          bottom-left = 6.0;
+          bottom-right = 6.0;
         };
         clip-to-geometry = true;
       }
