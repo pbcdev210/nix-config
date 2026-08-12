fef4693f948d545ca381fecc465f44443bafdeca
PBCDev <baochaupham4096@gmail.com>
2026-08-12 14:27:19 +0700

    niri(windown/mpvpaper): fix

--- home/desktops/niri/wallpaper/mpvpaper/default.nix
+++ home/desktops/niri/wallpaper/mpvpaper/default.nix
@@ -2,18 +2,15 @@
   programs.mpvpaper = {
     enable = true;
   };
-
-  programs.niri.settings = {
-    spawn-at-startup = [
-      {
-        argv = [
-          "mpvpaper"
-          "-o"
-          "no-audio loop panscan=1.0"
-          "*"
-          "/workspaces/nix-config/assets/kawaii-cat-girl.png"
-        ];
-      }
-    ];
-  };
+  programs.niri.settings.spawn-at-startup = [
+    {
+      argv = [
+        "mpvpaper"
+        "-o"
+        "no-audio --loop-inf"
+        "ALL"
+        "/workspaces/nix-config/assets/kawaii-cat-girl.png"
+      ];
+    }
+  ];
 }
