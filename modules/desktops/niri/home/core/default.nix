582bf2b283d4223f8d3beec6361b2fc3fa8e2569
PBCDev <baochaupham4096@gmail.com>
2026-08-16 01:44:54 +0700

    niri(core): systemd user services environment variables synchronization

--- modules/desktops/niri/home/core/default.nix
+++ modules/desktops/niri/home/core/default.nix
@@ -16,5 +16,11 @@
     MOZ_ENABLE_WAYLAND = "1";
   };
 
+  programs.niri.settings.spawn-at-startup = [
+    { command = [ "dbus-update-activation-environment" "--systemd" "WAYLAND_DISPLAY" "XDG_CURRENT_DESKTOP" "NIRI_ID" ]; }
+    { command = [ "systemctl" "--user" "import-environment" "WAYLAND_DISPLAY" "XDG_CURRENT_DESKTOP" "NIRI_ID" ]; }
+    { command = [ "systemctl" "--user" "restart" "graphical-session.target" ]; }
+  ];
+
   stylix.targets.niri.enable = true;
 }
