5e1cc1a039329205887918a21fef144d1e2f47e8
PBCDev <baochaupham4096@gmail.com>
2026-08-15 20:41:38 +0700

    vivaldi: setup args

--- modules/home/programs/vivaldi/default.nix
+++ modules/home/programs/vivaldi/default.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }:
+let
+  vivaldi-config = {
+    # commandLineArgs = "--ozone-platform-hint=auto --enable-features=UseOzonePlatform,VaapiVideoDecoder,CanvasOopRasterization --enable-gpu-rasterization";
+    commandLineArgs = "--ozone-platform-hint=auto --enable-features=UseOzonePlatform,VaapiVideoDecoder,CanvasOopRasterization --enable-gpu-rasterization --enable-features=WebUIDarkMode --force-dark-mode";
+  };
+in
 {
   home.packages = with pkgs; [
-    vivaldi
+    (vivaldi.override vivaldi-config)
     vivaldi-ffmpeg-codecs
   ];
 }
