053beed69a6f6a7f9562c39090a25f412357f94d
PBCDev <baochaupham4096@gmail.com>
2026-08-19 00:19:35 +0700

    nvim(edit) fix keymaps

--- modules/nixvim/edit/keymaps/default.nix
+++ modules/nixvim/edit/keymaps/default.nix
@@ -7,13 +7,38 @@
 
   programs.nixvim.keymaps = [
     {
-      mode = "i";
+      mode = [ "i" "c" ];
       key = "<C-BS>";
-      action = "<cmd>normal! db<CR>";
+      action = "<C-W>";
       options = {
         silent = true;
         desc = "Delete inner word with Ctrl+Backspace";
       };
+    }
+
+    {
+      mode = [ "n" "v" ];
+      key = "x";
+      action = ''"_x'';
+      options.desc = "Delete char into black hole";
+    }
+
+    {
+      mode = [ "n" "v" ];
+      key = "<leader>d";
+      action = ''"_d'';
+      options = {
+        silent = true;
+        desc = "Delete without copying to clipboard";
+      };
+    }
+
+    {
+      mode = "n";
+      key = "<Esc>";
+      action = "<cmd>nohlsearch<CR>";
+      options.silent = true;
+      options.desc = "Clear search highlight";
     }
   ];
 }
