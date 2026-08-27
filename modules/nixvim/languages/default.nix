2f1ce5590262adcfa637aad396ab962d02d01141
PBCDev <baochaupham4096@gmail.com>
2026-08-27 16:16:25 +0700

    nixvim(lang): restruct

--- modules/nixvim/languages/default.nix
+++ modules/nixvim/languages/default.nix
@@ -2,26 +2,27 @@
   imports = [
     ./core
 
-    ./bazel
-    ./c-cpp
-    ./cmake
-    ./cs
-    ./css
-    ./fs
-    ./html
-    ./java
-    ./json
-    ./lua
-    ./md
-    ./mdx
-    ./nix
-    ./nushell
-    ./python
-    ./rust
-    ./sh
-    ./ts-js
-    ./vim
-    ./xml
-    ./yml-yaml
+    ./bazel.nix
+    ./c-cpp.nix
+    ./cmake.nix
+    ./cs.nix
+    ./css.nix
+    ./fs.nix
+    ./html.nix
+    ./java.nix
+    ./json.nix
+    ./kanata.nix
+    ./lua.nix
+    ./md.nix
+    ./mdx.nix
+    ./nix.nix
+    ./nushell.nix
+    ./python.nix
+    ./rust.nix
+    ./sh.nix
+    ./ts-js.nix
+    ./vim.nix
+    ./xml.nix
+    ./yml-yaml.nix
   ];
 }
--- modules/nixvim/languages/vim/default.nix
+++
@@ -1,3 +0,0 @@
-{
-  imports = [ ./cmd.nix ];
-}
