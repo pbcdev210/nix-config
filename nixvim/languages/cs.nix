151522bcb7055ed279a152ea98d227a3451ab2c1
PBCDev <baochaupham4096@gmail.com>
2026-08-29 12:04:45 +0700

    nixvim(lang/cs): install dotnet deps

--- nixvim/languages/cs.nix
+++ nixvim/languages/cs.nix
@@ -1,12 +1,10 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   plugins.lsp.servers.roslyn_ls = {
     enable = true;
-
     package = pkgs.roslyn-ls;
   };
 
-  plugins.conform-nvim.settings.formatters_by_ft = { cs = "csharpier"; };
-
-  plugins.treesitter.settings.ensure_installed = [ "c_sharp" ];
+  plugins.conform-nvim.settings.formatters_by_ft = {cs = "csharpier";};
+  plugins.treesitter.settings.ensure_installed = ["c_sharp"];
+  extraPackages = with pkgs; [dotnet-sdk_10];
 }
