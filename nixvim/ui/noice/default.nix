e1d8df90fc4ba8a23ce9fb02f99ba4086cb971bb
PBCDev <baochaupham4096@gmail.com>
2026-08-29 12:11:11 +0700

    nixvim(noice): setup cmdline

--- nixvim/ui/noice/default.nix
+++ nixvim/ui/noice/default.nix
@@ -6,6 +6,40 @@
       cmdline = {
         enabled = true;
       };
+
+      popupmenu = {
+        enabled = false;
+      };
+
+      views = {
+        cmdline_popup = {
+          position = {
+            row = "98%";
+            col = "2%";
+          };
+          size = {
+            width = 70;
+            height = "auto";
+          };
+        };
+        popupmenu = {
+          relative = "editor";
+          position = {
+            row = 8;
+            col = "50%";
+          };
+          size = {
+            width = 70;
+            height = 10;
+          };
+          border = {
+            style = "rounded";
+            padding = [ 0 1 ];
+          };
+
+          win_options.winhighlight = "Normal:Normal,FloatBorder:DiagnosticInfo";
+        };
+      };
     };
   };
 }
