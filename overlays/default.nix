83336770171e5bfcc2b50220e3e09b64d4faf042
PBCDev <baochaupham4096@gmail.com>
2026-08-31 19:39:44 +0700

    overlays: setup auto load

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,6 +1,16 @@
-{ inputs, ... }:
-[
-  (import ./dotnet)
-  (import ./waycal { inherit inputs; })
-  (import ./nushell-plugins { inherit inputs; })
-]
+argv:
+let
+  dir = builtins.readDir ./.;
+  names = builtins.sort builtins.lessThan (builtins.attrNames dir);
+
+  nixFiles = builtins.filter (
+    name: dir.${name} == "regular" && builtins.match ".*\\.nix$" name != null && name != "default.nix"
+  ) names;
+
+  subdirs = builtins.filter (
+    name: dir.${name} == "directory" && builtins.pathExists (./. + "/${name}/default.nix")
+  ) names;
+
+  importFile = name: import (./. + "/${name}") argv;
+in
+(map importFile nixFiles) ++ (map importFile subdirs)
--- overlays/dotnet/default.nix
+++ overlays/dotnet/default.nix
@@ -1,3 +1,4 @@
+{ ... }:
 final: prev: {
   dotnet-sdk_10 = prev.dotnetCorePackages.sdk_10_0-bin;
   dotnet-runtime_10 = prev.dotnetCorePackages.runtime_10_0-bin;
