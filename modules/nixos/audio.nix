3a81f192349346b14a36a5b714891d0593253944
PBCDev <baochaupham4096@gmail.com>
2026-09-05 10:59:25 +0700

    audio: clean

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -1,10 +1,15 @@
 {
   services.pulseaudio.enable = false;
+
   services.pipewire = {
     enable = true;
     pulse.enable = true;
     jack.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
+    wireplumber.enable = true;
   };
+
+  security.rtkit.enable = true;
+  services.pipewire.audio.enable = true;
 }
