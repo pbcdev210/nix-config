7d4af1e5820a31d5d7eac8f2ee0c6f799df0efea
PBCDev <pbc210.dev@gmail.com>
2026-09-05 21:58:08 +0700

    zoxide: setup paths

--- home/programs/zoxide.nix
+++ home/programs/zoxide.nix
@@ -1,5 +1,20 @@
+{ pkgs, settings, ... }:
+let
+  zoxidePaths = [
+    settings.dirs.home
+    "${settings.dirs.home}/.config"
+
+    "/workspaces"
+    settings.dirs.nixConfig
+    settings.dirs.nixConfigBot
+  ];
+in
 {
   programs.zoxide = {
     enable = true;
   };
+
+  home.activation.seedZoxide = ''
+    ${builtins.concatStringsSep "\n" (map (path: "${pkgs.zoxide}/bin/zoxide add ${path}") zoxidePaths)}
+  '';
 }
